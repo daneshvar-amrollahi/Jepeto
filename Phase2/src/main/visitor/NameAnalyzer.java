3afe28fc0af7e2260fb7871711239b76fbcb5e53
daneshvar-amrollahi <amrollahi.daneshvar@gmail.com>
2021-05-13 20:06:00 +0430

    Added initial NameAnalyzer class

---
+++ Phase2/src/main/visitor/NameAnalyzer.java
@@ -0,0 +1,18 @@
+package main.visitor;
+
+import main.ast.nodes.Program;
+import main.ast.nodes.declaration.FunctionDeclaration;
+import main.ast.nodes.declaration.MainDeclaration;
+import main.ast.nodes.expression.*;
+import main.ast.nodes.expression.values.ListValue;
+import main.ast.nodes.expression.values.VoidValue;
+import main.ast.nodes.expression.values.primitive.BoolValue;
+import main.ast.nodes.expression.values.primitive.IntValue;
+import main.ast.nodes.expression.values.primitive.StringValue;
+import main.ast.nodes.statement.*;
+import java.util.Map;
+
+
+public class NameAnalyzer {
+
+}
